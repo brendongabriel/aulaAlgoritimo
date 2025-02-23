d62041dcb4e3185abeb7cd8e7cae1644ab28dc45
Brendon Gabriel Ribeiro da Silva <brendonsilva@weg.net>
2025-02-23 15:53:21 -0300

    Criação da rota de validação

--- index.js
+++ index.js
@@ -77,7 +77,9 @@ app.get('/sequence/debug', (req, res) => {
 
 // Rota para gerar sequência aleatória semelhante a um teclado virtual e armazená-la no banco de dados
 app.get('/sequence', (req, res) => {
-    let numbers = Array.from({ length: 10 }, (_, i) => i);
+    let numbers = Array.from({
+        length: 10
+    }, (_, i) => i);
     numbers.sort(() => Math.random() - 0.5);
     let sequence = [];
     for (let i = 0; i < numbers.length; i += 2) {
@@ -93,10 +95,81 @@ app.get('/sequence', (req, res) => {
             console.error("Erro ao inserir sequência:", err);
             res.status(500).send('Erro ao salvar sequência');
         } else {
-            res.json({ hash, sequence });
+            res.json({
+                hash,
+                sequence
+            });
         }
     });
     stmt.finalize();
+});
+
+// Rota para buscar sequência pelo hash
+app.get('/sequence/:hash', (req, res) => {
+    const hash = req.params.hash;
+    db.get('SELECT * FROM sequence WHERE hash = ?', [hash], (err, row) => {
+        if (err) {
+            res.status(500).send('Erro ao consultar dados');
+        } else if (row) {
+            res.json(row);
+        } else {
+            res.status(404).send('Sequência não encontrada');
+        }
+    });
+});
+
+// Rota para validar sequência de acordo com o que usuário digitou
+app.post('/sequence/validate', (req, res) => {
+    let error = false;
+    const {
+        hash,
+        sequence,
+        email
+    } = req.body;
+
+    if (!hash || !sequence || !email) {
+        res.status(400).send('Parâmetros inválidos');
+        return;
+    }
+    let user;
+    db.get('SELECT * FROM usuarios WHERE email = ?', [email], (err, row) => {
+        if (err) {
+            res.status(500).send('Erro ao consultar dados');
+        } else if (row) {
+            user = row;
+        } else {
+            res.status(404).send('Usuário não encontrado');
+            return;
+        }
+    });
+
+    db.get('SELECT * FROM sequence WHERE hash = ?', [hash], (err, row) => {
+        if (err) {
+            res.status(500).send('Erro ao consultar dados');
+        } else if (row) {
+            sequence.forEach((item, index) => {
+                if (!JSON.stringify(row.sequenceNumbers).includes(JSON.stringify(item))) {
+                    error = true;
+                } else {
+                    if (!item.includes(Number(user.senha[index]))) {
+                        error = true;
+                    }
+                }
+            });
+
+            // if (new Date().getTime() - row.time > 60000) {
+            //     error = true;
+            // }
+        } else {
+            res.status(404).send('Sequência não encontrada');
+        }
+
+        if (error) {
+            res.status(400).send('Usuário ou senha errada');
+        } else {
+            res.send('Bem vindo');
+        }
+    });
 });
 
 // Iniciar o servidor
